f99cb6395e7c94db46b2cd5c2de2c61d20349b7a
ssalasm <sebastiansalas@pm.me>
2022-04-27 10:17:40 -0400

    Se dan ejemplos sobre nuevas formas de concatenar cadenas, arreglos, y se explica diferencia entre var, let y const

--- src/es6/index.js
+++ src/es6/index.js
@@ -19,3 +19,44 @@ let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem = "Quisque justo augue, condimentum vel imperdiet bibendum, tempus eu leo \n"
++ "otra frase epica que necesitamos."
+
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+    'name' : 'oscar',
+    'age' : 32,
+    'country' : 'MX'
+}
+
+console.log(person.name, person.age, person.country);
+
+let {name, age, country} = person;
+console.log(name, age, country);
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+{
+    var globalVar = "gobalVar";
+}
+
+{
+    let globalLet = "globalLet";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = 'b';
+a = 'a';
