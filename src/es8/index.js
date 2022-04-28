f754c4e760dd80ac98f7aeacaa6a7bcffa8bee92
ssalasm <sebastiansalas@pm.me>
2022-04-27 22:53:38 -0400

    Ejemplo de algunas cosas que se incluyeron en es8

---
+++ src/es8/index.js
@@ -0,0 +1,25 @@
+const data = {
+    frontend: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana',
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+const data = {
+    frontend: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana',
+}
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
+
+
+const string = 'hello';
+console.log(string.padStart(7),'hi');
+console.log(string.padEnd(12),' -----');
+console.log('food'.padEnd(12),'  -----');
