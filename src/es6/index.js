79a64fe8cf84af543573e3ae7499e413bac5f8cc
ssalasm <sebastiansalas@pm.me>
2022-04-27 22:03:24 -0400

    Se agrega ejemplo de clases, módulos y generadores. Se modifica package.json por error en módulo

--- src/es6/index.js
+++ src/es6/index.js
@@ -113,3 +113,36 @@ helloPromise()
     .then(response => console.log(response))
     //.then(() => console.log('Hola'))
     .catch(error => console.log(error));
+
+class calculator{
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+
+import hello from './module.js';
+
+console.log(hello());
+
+function* helloWorld(){
+    if (true){
+        yield 'Hello, ';
+    }
+    if (true){
+        yield 'World ';
+    }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
